1323c4b47af4cce2512f7e6c7fe35648ac5153b6
Fiona Y <fyeung21@gmail.com>
2024-05-02 17:14:20 -0700

    Add basic fields to projectType

--- schemaTypes/projectType.ts
+++ schemaTypes/projectType.ts
@@ -6,8 +6,37 @@ export const projectType = defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'name',
+      name: 'project_name',
       type: 'string',
+      description: 'Name of the project',
+    }),
+    defineField({
+      name: 'short_description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'For text on project cards. Limit 160 characters',
+    }),
+    defineField({
+      name: 'long_description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description:
+        'Brief summary of project. Team? Roles? Timeframe? Design tools? Mention responsive mobile first approach.',
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      description: 'Cover image for project',
+    }),
+    defineField({
+      name: 'live_link',
+      type: 'url',
+      description: 'Link to live site',
+    }),
+    defineField({
+      name: 'github_link',
+      type: 'url',
+      description: 'Link to github repo',
     }),
   ],
 })
